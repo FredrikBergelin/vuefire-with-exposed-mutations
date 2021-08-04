f9564e474cf3b2e75d745eb8d6f43ec8d804ca9e
Fredrik Bergelin <fredrik.bergelin@gmail.com>
2021-08-05 00:45:19 +0200

    fix

--- packages/@fredrikbergelin/vuefire/src/firestore.ts
+++ packages/@fredrikbergelin/vuefire/src/firestore.ts
@@ -12,6 +12,10 @@ import Vue, { PluginFunction } from 'vue'
 const ops: OperationsType = {
   set: (target, key, value) => walkSet(target, key, value),
   add: (array, index, data) => array.splice(index, 0, data),
+  update: (array, oldIndex, newIndex, data) => {
+    array.splice(oldIndex, 1)
+    array.splice(newIndex, 0, data)
+  },
   remove: (array, index) => array.splice(index, 1),
 }
 
